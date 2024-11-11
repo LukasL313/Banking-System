97d3ad257c72634664564f16c7a356e46bd4f0d9
LukasLCas <122733748+Satishh1@users.noreply.github.com>
2024-11-11 14:42:59 +0100

    BankSystem

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           while (true)
+           {
+              Console.WriteLine("/n Hello, Welcome.");
+              Console.WriteLine("1. Create account.");
+              Console.WriteLine("2. Show account info.");
+              Console.WriteLine("3. Withdraw account.");
+              Console.WriteLine("4. Deposit into account.");
+              Console.WriteLine("5. Exit program");
+
+              int choice = int.Parse(Console.ReadLine());
+              
+              switch (choice)
+              {
+                
+                case '1':
+                 
+                break; 
+
+                 case '2':
+                
+                break; 
+
+                 case '3':
+                
+                break; 
+
+                 case '4':
+                
+                break; 
+
+                 case '5':
+                
+                return; 
+              }
+           }
+        }
+    }
+}
